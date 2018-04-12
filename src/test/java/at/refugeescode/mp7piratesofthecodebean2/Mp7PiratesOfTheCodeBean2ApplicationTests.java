357346fc237525a6adb4ea81ee38c3cfee43c72c
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-04-12 22:47:35 +0200

    Add project structure

---
+++ src/test/java/at/refugeescode/mp7piratesofthecodebean2/Mp7PiratesOfTheCodeBean2ApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp7piratesofthecodebean2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp7PiratesOfTheCodeBean2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
